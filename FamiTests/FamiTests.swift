f0154b0223f192643855cb039a3a0db8bda0903b
gokhyk <gokhankula@yahoo.com>
2025-08-08 14:22:08 -0500

    Initial Commit

---
+++ FamiTests/FamiTests.swift
@@ -0,0 +1,17 @@
+//
+//  FamiTests.swift
+//  FamiTests
+//
+//  Created by Ayse Kula on 8/4/25.
+//
+
+import Testing
+@testable import Fami
+
+struct FamiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
